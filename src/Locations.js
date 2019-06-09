703cc8a5434958d224da64f0f9cb15aeb858c64e
Giovanni Pereira dos Santos Oliveira <giovanni.psoliveira@gmail.com>
2019-06-09 13:45:31 -0300

    Add function to filter search (initial)

--- src/Locations.js
+++ src/Locations.js
@@ -1,10 +1,40 @@
-import React,  {Component} from "react";
+import React, { Component } from "react";
+import { DebounceInput } from 'react-debounce-input';
 import "./Locations.css";
 
 
 class Locations extends Component {
     constructor(props) {
         super(props);
+
+        this.allStations = props.data;
+
+        this.state = {
+            stations: [].concat(props.data)
+        }
+    }
+
+    changeStation(stationActive) {
+        this.setState(({ stations }) => {
+            const activeStation = station => {
+                station.pressed = stationActive === station;
+
+                return station;
+            };
+
+            return { stations: stations.map(activeStation) };
+        });
+
+        this.props.activeStation(stationActive);        
+    }
+
+    filterStations(query) {
+        const stationsFiltered = this.allStations.filter( ({title}) => title.includes(query));        
+
+        this.props.changeStation(stationsFiltered);
+        this.setState({
+            stations: stationsFiltered
+        });
     }
 
     render() {
@@ -13,6 +43,38 @@ class Locations extends Component {
                 <h2 className="locations-title">
                     {this.props.title}
                 </h2>
+
+                <div className="locations-filter">
+                    <DebounceInput
+                        placeholder="Station Location"
+                        className="locations-filter-input"
+                        alt="Station Location"
+                        debounceTimeout={300}
+                        onChange={({ target }) => this.filterStations(target.value)} />
+
+                    <button className="locations-filter-button">
+                        Filter
+                    </button>
+                </div>
+
+                <ul className="locations-wrapper">
+                    {
+                        this.state.stations.map(item => (
+                            <li
+                                key={item.location.venueId}
+                                tabIndex="0"
+                                className="location"
+                                role="button"
+                                aria-pressed={item.pressed || false}
+                                onClick={() => this.changeStation(item)}
+                            >
+
+                                {item.title}
+
+                            </li>
+                        ))
+                    }
+                </ul>
             </div>
         );
     }
