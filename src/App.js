80e29eb7ad4e471d4061ff9e3b492214ff0eec48
Giovanni Pereira dos Santos Oliveira <giovanni.psoliveira@gmail.com>
2019-06-09 13:46:38 -0300

    Add Locations properties

--- src/App.js
+++ src/App.js
@@ -7,16 +7,29 @@ import stations from "./stations.json";
 
 class NeighborhoodApp extends Component {
     constructor() {
+        super();
+
         this.states = {
             stations
         };
+    }
+
+    updateMap(filteredStations) {
+        console.log(filteredStations);
+    }
+
+    activeStation(station) {
+        console.log(station);
     }
 
     render() {
         return (
             <div className="app">
                 <Header/>
-                <Locations title={'Estações de São Paulo'}/>
+                <Locations 
+                    changeStation={filteredStations => this.updateMap(filteredStations)}
+                    activeStation={station => this.activeStation(station)}
+                    data={this.states.stations} title={'Estações de São Paulo'}/>
             </div>
         );
     }
