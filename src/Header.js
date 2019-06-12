cde8a47f65605fb8b553937819a8602bc4d7b49a
Giovanni Pereira dos Santos Oliveira <giovanni.psoliveira@gmail.com>
2019-06-12 08:15:32 -0300

    Add button to show credits of the resources used

--- src/Header.js
+++ src/Header.js
@@ -3,12 +3,26 @@ import './Header.css'
 
 
 class Header extends Component {
+
+    showCpyrightOfFeatures() {
+        alert(`
+        A lista de estações originalmente foi obtida através do reposiório https://gist.github.com/rafaelrinaldi/6a82dd1eceed6dfc7deb
+
+        O ícone do menu e do ponteiro do mapa foi obtido através de Icons made by Smashicons (https://www.flaticon.com/authors/smashicons) do site https://www.flaticon.com/, licensiado por is licensed by [CC 3.0 BY](http://creativecommons.org/licenses/by/3.0/)`);
+    }
+
     render() {
         return (
             <header className="header">
                 <button 
                     className="header-button" 
                     onClick={() => this.props.clickMenu()}>
+                </button>
+
+                <button 
+                    className="header-resources"
+                    onClick={() => this.showCpyrightOfFeatures()}>
+                    Recursos <br/>utilizados
                 </button>
             </header>
         )
